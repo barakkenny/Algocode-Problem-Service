cf1be99dcd25133cfbec2842309f95981179de30
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-05-10 08:46:43 +0100

    Feat: Added routing structure for versioned API for problems

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { PORT } = require('./config/server.config');
 const bodyParser = require('body-parser');
+const apiRouter = require('./src/appRoutes')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.json({message: 'Server is running'});
---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,33 @@
+ function addProblem(req, res) {
+
+ }
+
+ function pingProblemController(req, res) {
+    return res.json({message: 'ping controller is up'})
+ }
+
+
+ function getProblem(req, res) {
+
+ }
+
+ function getProblems(req, res) {
+
+ }
+
+ function deleteProblem(req, res) {
+
+ }  
+
+function updateProblem(req, res) {
+            
+}
+
+module.exports = {
+    addProblem,
+    getProblem,
+    getProblems,
+    deleteProblem,
+    updateProblem,
+    pingProblemController
+}

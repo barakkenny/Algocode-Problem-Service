2e59bc9500dcdeb519d60625ecfdee44064d98f9
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-05-13 11:31:35 +0100

    Feat: Added mongoose db config

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const apiRouter = require("./src/appRoutes");
 const BaseError = require("./src/errors/base.error");
 const errorHandler = require("./src/utils/errorHandler");
 const { StatusCodes } = require('http-status-codes')
+const connectDB = require("./config/db.config");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -22,7 +24,14 @@ app.get("/ping", (req, res) => {
 //Last Error handler middleware
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`listening at PORT ${PORT}`);
-  // throw new BaseError("not found", StatusCodes.NOT_FOUND, "Resource not found", {});
+  await connectDB();
+  console.log("connected to the database");
+
+
+  const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 });
